153b6598a1a2cd3d84f6a611931401ea3943e492
CatherineZM <catherine.zm@outlook.com>
2022-05-09 00:35:40 -0400

    Initialized repo

---
+++ src/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        {/* Complete your workshop in here*/}
+        <div>Welcome to FUBU React Workshop!</div>
+      </div>
+    )
+  }
+}
+
+export default HomePage;
